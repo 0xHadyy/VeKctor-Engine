67c76b4b9eb94f6039a0aafc8bed2b0a03987b2a
hady <abdelhadinodejs@gmail.com>
2026-02-10 18:58:59 +0100

    fix: correct inverted indexting with title and abstract freqs

--- src/inverted_index.py
+++ src/inverted_index.py
@@ -1,60 +1,50 @@
-# Create inverted indexing from the tokinized text of arXiv_XXXX.json
-# Simple first sketch implementation
-
-# term1 -> [1][3][7]
-
-from typing import Counter
-from tokenizer import Tokenizer
-from collections import defaultdict, Counter
-
-from arxiv_client import ArxivClient
+from src.tokenizer import Tokenizer
+from collections import defaultdict
 
 
-class inverted_index:
+class invertedIndex:
     def __init__(self):
-        self.tokinizer = Tokenizer()
+        self.tokenizer = Tokenizer()
         # Using default dict for dynamique assigning
         self.inverted_index = defaultdict(dict)
         self.papers = {}
         self.next_id = 0
+        self.docIDs_list = []
 
-    def inverted_indexer(self, paper):
+    def get_posting_list(self, term):
+        return self.inverted_index[term]
+
+    def get_docIDs(self):
+        return self.docIDs_list
+
+    def inverted_indexer(self, paper: dict) -> dict:
         paper_id = self.next_id
         self.papers[paper_id] = {
+            "id": paper["id"],
             "title": paper["title"],
-            "abstrat": paper["abstract"],
-            "authors": paper["authors"],
-            "arxiv_id": paper["arxiv_id"],
-            "pdf_url": paper["pdf_url"],
+            "abstract": paper["abstract"],
+            "length": len(paper["title"]) + len(paper["abstract"]),
         }
-        title_tokens = self.tokinizer.tokenize(paper["title"])
-        summary_tokens = self.tokinizer.tokenize(paper["abstract"])
+        title_tokens = self.tokenizer.tokenize(paper["title"])
+        summary_tokens = self.tokenizer.tokenize(paper["abstract"])
+        # count the tokens
 
-        frq = 1
-        for title_token in title_tokens:
-            if paper_id in self.inverted_index[title_token]:
-                self.inverted_index[title_token][paper_id]["frequency"] += 1
-                # the token is already in the inverted index  but the paper_id doesnt exist
-                print(
-                    f"Tryin to add the token '{title_token}' to the paper_id : <{paper_id}>, that already exist "
-                )
-            else:
-                paper_info = {"frequency": frq}
-                self.inverted_index[title_token][paper_id] = {"frequency": 1}
-                print(f"added  the token :'{title_token}' to paper id : <{paper_id}> ")
+        token_count = defaultdict(lambda: {"title": 0, "abstract": 0})
+
+        for token in title_tokens:
+            token_count[token]["title"] += 1
+
+        for token in summary_tokens:
+            token_count[token]["abstract"] += 1
+
+        for token, count in token_count.items():
+            # creating new entry
+            self.inverted_index[token][paper_id] = {
+                "title_count": count["title"],
+                "abstract_count": count["abstract"],
+                "total_freq": count["title"] + count["abstract"],
+            }
+
+        self.docIDs_list.append(self.next_id)
         self.next_id += 1
         return self.inverted_index
-
-
-inv = inverted_index()
-
-arXiv = ArxivClient(max_results=4)
-docs = arXiv.search("IA")
-filepath = arXiv.save_papers(docs)
-papers = arXiv.load_json(filepath)
-# print(paper[0]["title"])
-for paper in papers:
-    inverted_index = inv.inverted_indexer(paper)
-
-for key, value in inverted_index.items():
-    print(f"{key}:{value}\n")
